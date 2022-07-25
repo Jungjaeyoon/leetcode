ccb66b1f13367ed1ec852890af5463b5b9236952
mzza-jung <aragon5198@gmail.com>
2022-07-25 19:14:38 +0900

    Time: 6272 ms (5.01%), Space: 33.5 MB (5.06%) - LeetHub

--- 547-number-of-provinces/547-number-of-provinces.py
+++ 547-number-of-provinces/547-number-of-provinces.py
@@ -3,19 +3,22 @@ class Solution:
     
         c = len(isConnected)
         cn = len(isConnected[0])
-        cnt = 0
-
+        cnt = 0 
+        passlist = []
+            
         def checker(x):
             connectedCity = [ z for z,y in enumerate(isConnected[x]) if y != 0]
             for pos in connectedCity:
                 isConnected[x][pos] = 0 
+                passlist.append(pos)
                 checker(pos)
 
 
 
         for x in range(c):
-            if sum(isConnected[x]) > 0:
-                cnt+=1
-            checker(x)
+            if x not in passlist:
+                if sum(isConnected[x]) > 0:
+                    cnt+=1
+                checker(x)
             
         return cnt
