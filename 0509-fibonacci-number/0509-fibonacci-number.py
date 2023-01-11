11c727fd25bcdf4ffa4d0526b4d5b5834ff13a92
mzza-jung <aragon5198@gmail.com>
2023-01-11 15:55:02 +0900

    Time: 36 ms (77.98%), Space: 13.9 MB (52.01%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -1,7 +1,17 @@
 class Solution:
+    fibolist = []
     def fib(self, n: int) -> int:
+        if len(self.fibolist) == 0:
+            self.fibolist =[0,1]+[0 for x in range(n+1)]
         if n==0: 
+            self.fibolist[0] = 0
             return 0
         if n==1: 
+            self.fibolist[1] = 1
             return 1
-        return self.fib(n-1) + self.fib(n-2)
+        
+        if self.fibolist[n] == 0:
+            self.fibolist[n] = self.fib(n-1) + self.fib(n-2)
+            return self.fibolist[n]            
+        else:
+            return self.fibolist[n]
